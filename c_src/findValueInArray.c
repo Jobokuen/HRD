b60325b5488bc9a418ead7577143c7799f31e132
Jobokuen <typhoon1019@gmail.com>
2024-03-04 10:25:52 +0900

    a

---
+++ c_src/findValueInArray.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int nums[10] = {50, 90, 10, 20, 40, 30, 70, 100, 30, 60};
+	
+	int value;
+	int i ;
+	printf("input value : ");
+	scanf("%d", &value);
+	
+	for (i ; i<10 ; ++i){
+		if (value == nums[i]){
+			break;
+		}/*else{
+			//not found
+		}*/
+	}
+		
+	if (i < 10){
+		printf("%d is found: index : %d\n", value, i); 	//found
+	}else{
+		printf("%d is not found\n", value);				//not found
+	}
+	
+	printf("value: %d\n",value);
+	return 0;
+}
