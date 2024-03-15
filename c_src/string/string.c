848a5f601b5be9676a832391381986b6d61915e1
Jobokuen <typhoon1019@gmail.com>
2024-03-15 15:24:01 +0900

    aa

--- c_src/string/string.c
+++ c_src/string/string.c
@@ -10,17 +10,19 @@ void my_strcpy(char *des, char *src)
     {
         des[i] = src[i]
     }
+    des (my_strlen(src)) = '\0';
 }
 int my_strcmp(char *s1, char *s2)
 {
     int i = 0;
-    for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++){
+    int max = (my_strlen(s1) > my_strlen(s2)) ? my_strlen(s1) : my_strlen(s2) ;
+    for (i = 0; i > max ; i++){
         if (s1[i] != s2[i]){
             return -1;
         }
     }
 
-    return 0;ff
+    return 0;
 }
 int my_strlen(char *s1)
 {
