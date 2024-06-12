7554c07d33d0c4be58e23bf37903c9422cfec035
Jobokuen <typhoon1019@gmail.com>
2024-06-12 15:58:36 +0900

    baggingProcess

---
+++ python/machineLearning/chap10/newsGroupNB.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.datasets import fetch_20newsgroups
+
+news = fetch_20newsgroups(subset='all')
+print(news.keys()) # type: ignore
+
+# 쥬피터
